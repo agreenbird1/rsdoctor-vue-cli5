2f3607114099697dc2a3153937f6c1d2a902b948
QiTao Tang <tangqitao@bluepulse.cn>
2024-12-11 17:07:57 +0800

    添加@rsdoctor/webpack-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
+const { RsdoctorWebpackPlugin } = require("@rsdoctor/webpack-plugin");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    plugins: [new RsdoctorWebpackPlugin()],
+  },
 });
